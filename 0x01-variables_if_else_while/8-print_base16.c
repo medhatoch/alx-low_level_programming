e8f1574d78cdb15913736bf5852c6c09696e9542
medhatoch <medamine.hatoch@gmail.com>
2023-03-17 10:13:59 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char j;
+for ( i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (j = 'a'; j <= 'f'; j-++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
