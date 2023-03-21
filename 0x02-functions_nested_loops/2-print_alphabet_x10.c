d7a609a3e9702d0a66d56ae8be3063bfcea902b5
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 15:35:04 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - check the code
+*
+* no return
+*/
+
+void print_alphabet_x10(void) {
+char letter = 'a';
+while (i<10)
+{
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+}
