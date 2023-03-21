29b36852d6ba049afee6300750677bdca3507073
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 08:47:48 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**                                                                                                 
+ * main - debugging example                                                                         
+ * Return: 0                                                                                        
+*/
+int main(void){
+printf("_putchar\n");
+return (0);
+}
