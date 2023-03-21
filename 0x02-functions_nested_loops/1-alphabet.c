2aa15c4b16691fc5fe68c41c2880628c5f914fd2
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 13:44:36 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,10 @@
 #include "main.h"
+
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-void print_alphabet(void)
-{
-char c;
-for(c='a'; c<='z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-}
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet(void)
+{
+char c;
+for(c='a'; c<='z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
