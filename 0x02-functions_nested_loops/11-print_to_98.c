a1b6d693bd4a17e1a42239318dbadf28b2286a76
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 19:23:40 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+
+_putchar('9');
+_putchar('8');
+_putchar('\n');
+}
