322bc261bda0454bcf358e82d248b26549ed74fa
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 18:32:45 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,18 @@
 #include "main.h"
 /**
 * _islower - check the code
-*
-* return 1 or 0
+* c is a letter
+* return 1 if c is lowercase else return 0
 */
 
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
