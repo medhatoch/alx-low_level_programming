ca94d76083528db2056fa5a93a0ead811d1e4c31
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 19:11:14 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,10 @@ int print_last_digit(int n)
 int last_digit;
 
 /* Get the last digit of n */
-last_digit = _abs(n % 10);
+if (n < 0)
+last_digit = -1 * (n % 10);
+else
+last_digit = n % 10;
 
 /* Print the last digit */
 _putchar(last_digit + '0');
