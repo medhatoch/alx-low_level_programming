4e4a31e906fd1056c46be41cf84019aa253336f9
medhatoch <medamine.hatoch@gmail.com>
2023-03-21 18:57:34 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,6 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number to check
- *
  * Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
@@ -13,13 +12,13 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-} 
-else if (n == 0) 
+}
+else if (n == 0)
 {
 _putchar('0');
 return (0);
-} 
-else 
+}
+else
 {
 _putchar('-');
 return (-1);
