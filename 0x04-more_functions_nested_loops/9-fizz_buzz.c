729f1699350b73553bea29e37d0683cd4635467f
medhatoch <medamine.hatoch@gmail.com>
2023-03-23 15:49:42 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,6 @@
 /**
 * main - Entry point
 *
-* Description: Prints the numbers from 1 to 100, replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz". Each number or word is separated by a space, and the entire sequence ends with a new line character.
-*
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -12,21 +10,14 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
 printf("FizzBuzz ");
-}
 else if (i % 3 == 0)
-{
 printf("Fizz ");
-}
 else if (i % 5 == 0)
-{
 printf("Buzz ");
-}
 else
-{
 printf("%d ", i);
 }
-}
+printf("\n");
 return (0);
 }
