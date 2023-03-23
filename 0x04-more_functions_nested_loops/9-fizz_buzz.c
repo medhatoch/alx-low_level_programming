9d578707836d4b758280077428b0d19d21348ee7
medhatoch <medamine.hatoch@gmail.com>
2023-03-23 15:08:23 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Prints the numbers from 1 to 100, replacing multiples of 3 with
- *              "Fizz", multiples of 5 with "Buzz", and multiples of both 3 and
- *              5 with "FizzBuzz". Each number or word is separated by a space,
- *              and the entire sequence ends with a new line character.
- *
- * Return: Always 0 (Success)
- */
-
+* main - Entry point
+*
+* Description: Prints the numbers from 1 to 100, replacing multiples of 3 with
+* "Fizz", multiples of 5 with "Buzz", and multiples of both 3 and
+* 5 with "FizzBuzz". Each number or word is separated by a space,
+* and the entire sequence ends with a new line character.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i;
