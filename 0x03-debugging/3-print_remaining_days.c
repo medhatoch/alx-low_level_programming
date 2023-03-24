c8a01863455581b9dbbfc812d40da38926537ba5
medhatoch <medamine.hatoch@gmail.com>
2023-03-23 17:42:56 -0700

    0x03. C - Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,64 +9,27 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_feb = 28;
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int i, total_days = 0;
 
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-    {
-        days_in_feb = 29;
+    for (i = 1; i < month; i++) {
+        total_days += days_in_month[i];
     }
 
-    if (month > 2 && days_in_feb == 28)
-    {
-        day--;
-    }
+    total_days += day;
 
-    switch (month)
-    {
-        case 1:
-            day = 31 + day;
-            break;
-        case 2:
-            day = 31 + day;
-            break;
-        case 3:
-            day = 59 + day;
-            break;
-        case 4:
-            day = 90 + day;
-            break;
-        case 5:
-            day = 120 + day;
-            break;
-        case 6:
-            day = 151 + day;
-            break;
-        case 7:
-            day = 181 + day;
-            break;
-        case 8:
-            day = 212 + day;
-            break;
-        case 9:
-            day = 243 + day;
-            break;
-        case 10:
-            day = 273 + day;
-            break;
-        case 11:
-            day = 304 + day;
-            break;
-        case 12:
-            day = 334 + day;
-            break;
-        default:
-            break;
-    }
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        if (month > 2) {
+            total_days += 1;
+        }
 
-    printf("Day of the year: %d\n", day);
-    printf("Remaining days: %d\n", 365 + (days_in_feb == 29) - day);
+        printf("Day of the year: %d\n", total_days);
+        printf("Remaining days: %d\n", 366 - total_days);
+    } else {
+        printf("Day of the year: %d\n", total_days);
+        printf("Remaining days: %d\n", 365 - total_days);
+    }
 }
 
