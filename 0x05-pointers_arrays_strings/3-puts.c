39d1f92371029e48518480e86c08457cd47f4df8
medhatoch <medamine.hatoch@gmail.com>
2023-03-27 11:18:33 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout, followed by a new line.
+ * @str: pointer to a character string.
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+
+_putchar('\n');
+}
