8c0d34b56f6568e6d900c614b8bee08f3ecd2cb8
medhatoch <medamine.hatoch@gmail.com>
2023-04-05 14:40:54 -0700

    0x07-pointers_arrays_strings

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,33 @@
 #include "main.h"
 /**
- * find_sqrt - helper function to find the natural square root of a number.
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
  *
- * @n: the number to find the square root of.
- * @start: the starting point of the search.
- * @end: the ending point of the search.
- *
- * Return: the square root of n, or -1 if n does not have a natural square root.
- */
-int find_sqrt(int n, int start, int end)
-{
-if (start > end)
-return (-1);
-if (start * start == n)
-return (start);
-if (start * start > n)
-return (-1);
-return (find_sqrt(n, start + 1, end));
-}
-
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- *
- * @n: the number to find the square root of.
- *
- * Return: the square root of n, or -1 if n does not have a natural square root.
+ * Return: The natural square root of @n, or -1 if @n does not have a
+ * natural square root.
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-return (find_sqrt(n, 1, n / 2));
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt_helper(n, 1));
+}
+
+/**
+ * _sqrt_helper - Finds the natural square root of a number.
+ * @n: The number to find the square root of.
+ * @i: The current guess.
+ *
+ * Return: The natural square root of @n, or -1 if @n does not have a
+ * natural square root.
+ */
+int _sqrt_helper(int n, int i)
+{
+if (i * i == n)
+return (i);
+if (i * i > n)
+return (-1);
+return (_sqrt_helper(n, i + 1));
 }
