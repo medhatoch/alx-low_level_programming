3c887e4f66aa6d987ab446fb8fe776483e9b7d36
medhatoch <medamine.hatoch@gmail.com>
2023-04-05 14:36:24 -0700

    0x07-pointers_arrays_strings

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,44 +1,34 @@
 #include "main.h"
-
-int find_sqrt(int n, int min, int max);
+/**
+ * find_sqrt - helper function to find the natural square root of a number.
+ *
+ * @n: the number to find the square root of.
+ * @start: the starting point of the search.
+ * @end: the ending point of the search.
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root.
+ */
+int find_sqrt(int n, int start, int end)
+{
+if (start > end)
+return (-1);
+if (start * start == n)
+return (start);
+if (start * start > n)
+return (-1);
+return (find_sqrt(n, start + 1, end));
+}
 
 /**
  * _sqrt_recursion - returns the natural square root of a number.
+ *
  * @n: the number to find the square root of.
  *
- * Return: the natural square root of @n, or -1 if @n does not have a natural
- * square root.
+ * Return: the square root of n, or -1 if n does not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-
-return find_sqrt(n, 1, n / 2);
-}
-
-/**
- * find_sqrt - finds the natural square root of a number within a given range.
- * @n: the number to find the square root of.
- * @min: the minimum possible square root of @n.
- * @max: the maximum possible square root of @n.
- *
- * Return: the natural square root of @n, or -1 if @n does not have a natural
- * square root.
- */
-int find_sqrt(int n, int min, int max)
-{
-if (min > max)
-return (-1);
-
-int guess = (min + max) / 2;
-int square = guess * guess;
-
-if (square == n)
-return (guess);
-
-if (square < n)
-return (find_sqrt(n, guess + 1, max));
-
-return (find_sqrt(n, min, guess - 1));
+return (find_sqrt(n, 1, n / 2));
 }
