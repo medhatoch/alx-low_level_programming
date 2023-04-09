c0ed2666fd4fd028c0df1a1ddf91f6033b786a8a
medhatoch <medamine.hatoch@gmail.com>
2023-04-09 16:41:02 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,7 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -14,9 +17,12 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, size_t n);
+char *_memcpy(char *dest, char *src, size_t n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
+size_t _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
+
