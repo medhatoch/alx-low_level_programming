5a67e0d84881744561d55c39de6d2af8d8c41eff
medhatoch <medamine.hatoch@gmail.com>
2023-04-10 10:49:20 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(int argc, char *argv[])
+/**
+ * print_program_name - prints the name of the program
+ * @program_name: the name of the program
+ *
+ * Return: void
+ */
+void print_program_name(char *program_name)
 {
-printf("%s\n", argv[0]);
-return (0);
+while (*program_name)
+{
+_putchar(*program_name);
+program_name++;
+}
+_putchar('\n');
 }
