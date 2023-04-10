3193b557b954019e80e57b35c6fde26e8006222b
medhatoch <medamine.hatoch@gmail.com>
2023-04-10 10:12:07 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_program_name - prints the name of the program
+ * @program_name: the name of the program as passed in the argv array
+ */
+void print_program_name(char *program_name)
+{
+printf("%s\n", program_name);
+}
