0808b37de6a70b0a9562c74fcb42cff17ea30989
medhatoch <medamine.hatoch@gmail.com>
2023-04-10 10:10:52 -0700

    0x0A-argc_argv

--- main.h
+++ main.h
@@ -1,10 +1,3 @@
 #include <stdio.h>
 
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-
+void print_program_name(char *program_name);
