43aa626ec835ab4c87ec571e64b33315b0900fd7
medhatoch <medamine.hatoch@gmail.com>
2023-04-13 17:16:30 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	unsigned int i;
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -36,12 +37,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 	{
-		for (unsigned int i = 0; i < old_size; i++)
+		for (i = 0; i < old_size; i++)
 			*((char *)new_ptr + i) = *((char *)ptr + i);
 	}
 	else
 	{
-		for (unsigned int i = 0; i < new_size; i++)
+		for (i = 0; i < new_size; i++)
 			*((char *)new_ptr + i) = *((char *)ptr + i);
 	}
 
