8be2ab56187fbf306989e33d8b18278815593410
medhatoch <medamine.hatoch@gmail.com>
2023-05-08 15:45:58 -0700

    0x15-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Constants */
+#define MAX_NUM 100
+
+#endif /* MAIN_H */
