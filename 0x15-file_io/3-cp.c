fbf17a228f57ac5ae393ec25d84965d35911785e
medhatoch <medamine.hatoch@gmail.com>
2023-05-08 16:24:06 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,64 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "copy_file.c"
 
-#define BUFSIZE 1024
+/**
+* main - Entry point for the program
+* @argc: The number of arguments passed to the program
+* @argv: An array of strings containing the arguments passed to the program
+*
+* Return: 0 on success, 97-100 on error
+*/
 
 int main(int argc, char **argv)
 {
-int fd_from, fd_to, ret;
-ssize_t nread;
-char buf[BUFSIZE];
-
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
+if (copy(argv[1], argv[2]) == 1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
-}
-
-while ((nread = read(fd_from, buf, BUFSIZE)) > 0)
-{
-ret = write(fd_to, buf, nread);
-if (ret != nread)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-}
-
-if (nread == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
 }
 
 return (0);
 }
-
