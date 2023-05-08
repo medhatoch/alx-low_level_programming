769cc0be6e2a46fe744fae19df8ea2b219fb6d9b
medhatoch <medamine.hatoch@gmail.com>
2023-05-08 15:55:14 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,18 +19,15 @@ char *buffer;
 
 if (filename == NULL)
 return (0);
-
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-
 buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
-
 bytes_read = read(fd, buffer, letters);
 if (bytes_read == -1)
 {
@@ -38,7 +35,6 @@ close(fd);
 free(buffer);
 return (0);
 }
-
 buffer[bytes_read] = '\0';
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
@@ -47,7 +43,6 @@ close(fd);
 free(buffer);
 return (0);
 }
-
 close(fd);
 free(buffer);
 return (bytes_read);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 /* Constants */
 #define MAX_NUM 100
