9d2e6526e39ff4c9d207759bedbb366772ce56df
root <medamine.hatoch@gmail.com>
2023-07-05 19:01:30 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,25 +11,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *current;
-unsigned int count = 0;
+listint_t *new_node, *temp;
+unsigned int i = 0;
+
 if (head == NULL)
 return (NULL);
-if (idx == 0)
-return (add_nodeint(head, n));
-current = *head;
-while (current != NULL && count < idx - 1)
-{
-current = current->next;
-count++;
-}
-if (current == NULL)
-return (NULL);
+
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
+
 new_node->n = n;
-new_node->next = current->next;
-current->next = new_node;
+
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+
+temp = *head;
+while (temp != NULL && i < idx - 1)
+{
+temp = temp->next;
+i++;
+}
+
+if (temp == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
+new_node->next = temp->next;
+temp->next = new_node;
+
 return (new_node);
 }
