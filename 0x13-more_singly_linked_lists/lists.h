74c41dc8c2b222611d6c103708532a4689ba707c
root <medamine.hatoch@gmail.com>
2023-07-05 18:17:24 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ struct listint_s *next;
 } listint_t;
 
 /* Function prototype */
+listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 #endif /* LISTS_H */
